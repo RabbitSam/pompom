bc001ce81135ede4b4e57e207867027421d21b8f
RabbitSam <aquibmahmood1637@gmail.com>
2024-06-26 15:08:17 -0400

    Added introduction text to home page

--- src/renderer/home/Home.tsx
+++ src/renderer/home/Home.tsx
@@ -7,11 +7,38 @@ export default function Home() {
     return (
         <PageContainer className={styles.main}>
             <h1>Welcome to POMPOM!</h1>
-            <p>
-                Whether you're looking to work hard or work smart, using the pomodoro method will get you on top.
-                <br />
-                Insert other bs here
-            </p>
+            <div className={styles.content}>
+                <p>
+                    Whether you're looking to work hard or work smart, using the pomodoro method will get you on top.
+                </p>
+                <p>
+                    Created by Francesco Cirillo in the late 1980s, the pomodoro method has become a well known and widely used
+                    method of productivity. It primarily consists of <strong>seven</strong> steps:
+                </p>
+                <ol>
+                    <li>
+                        Decide on a task
+                    </li>
+                    <li>
+                        Set the Pomodoro timer (typically for 25 minutes).
+                    </li>
+                    <li>
+                        Work on the task.
+                    </li>
+                    <li>
+                        When the timer ends, take a short break (typically 5 - 10 minutes).
+                    </li>
+                    <li>
+                        When the break time ends, get started again from step 2.
+                    </li>
+                    <li>
+                        After four pomodoros are done, take a longer break (typically 20 to 30 minutes).
+                    </li>
+                    <li>
+                        When the long break is over, get started again from step 2.
+                    </li>
+                </ol>
+            </div>
             <div className={styles.buttonContainer}>
                 <ButtonLink category="primary" to="/quick-pom">
                     Get Started with a Quick Pom
